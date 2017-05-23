e6b7d66eb0ced03390590b24bb80d299f263a2b2
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2017-05-23 12:02:55 +0200

    Update n26.rb

--- n26.rb
+++ n26.rb
@@ -1,5 +1,5 @@
-class Delta < Formula
-  desc "diff tool with browser-based GUI"
+class N26 < Formula
+  desc "CLI tool for N26 Bank"
   homepage "https://github.com/guitmz/n26"
   url "https://github.com/guitmz/n26/releases/download/1.0.0/n26-cli-1.0.0_darwin_amd64.zip"
   sha256 "de865d194301635abfd93ff126177e2d449316ff031b3ecead564ca6e8c38d77"
