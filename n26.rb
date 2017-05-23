d1e3dacd336a76805ef6c763d76c91de0b6c8194
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2017-05-23 12:00:26 +0200

    Create n26.rb

---
+++ n26.rb
@@ -0,0 +1,16 @@
+class Delta < Formula
+  desc "diff tool with browser-based GUI"
+  homepage "https://github.com/guitmz/n26"
+  url "https://github.com/guitmz/n26/releases/download/1.0.0/n26-cli-1.0.0_darwin_amd64.zip"
+  sha256 "de865d194301635abfd93ff126177e2d449316ff031b3ecead564ca6e8c38d77"
+
+  bottle :unneeded
+
+  def install
+    bin.install "n26"
+  end
+
+  test do
+    system "#{bin}/n26", "--version"
+  end
+end
