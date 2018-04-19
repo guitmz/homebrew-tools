81e6bc2b0fb9df429c1ce8507cebdc124febd01d
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2018-04-19 11:09:11 +0200

    Update n26.rb

--- n26.rb
+++ n26.rb
@@ -1,8 +1,8 @@
 class N26 < Formula
   desc "CLI tool for N26 Bank"
   homepage "https://github.com/guitmz/n26"
-  url "https://github.com/guitmz/n26/releases/download/1.3.0/n26-cli-1.3.0-darwin-amd64.zip"
-  sha256 "a5e987f4ee0740a2638bbe426fc1bf7f8eb1fd2a6af913b21b67386dccbf318d"
+  url "https://github.com/guitmz/n26/releases/download/1.4.0/n26-cli-1.4.0-darwin-amd64.zip"
+  sha256 "1d910b0cd01e770f3ba53086e3134c35efe9e4c47f6d8849c380195909e2da2b"
 
   bottle :unneeded
 
