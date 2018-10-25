98c3a591c0c1c4c4195a34ed3f5a7cc0a53b70f7
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-25 11:26:25 +0200

    Brew formula update for n26 version 1.4.6

--- n26.rb
+++ n26.rb
@@ -1,9 +1,9 @@
 class N26 < Formula
   desc "CLI tool for N26 Bank"
   homepage "https://github.com/guitmz/n26"
-  url "https://github.com/guitmz/n26/releases/download/1.4.5/n26-cli-1.4.5-darwin-amd64.zip"
-  version "1.4.5"
-  sha256 "8593a787b139dae49c306ed31f236dd87b725c07a1811a8c0106c92f0cf4123c"
+  url "https://github.com/guitmz/n26/releases/download/1.4.6/n26-cli-1.4.6-darwin-amd64.zip"
+  version "1.4.6"
+  sha256 "082e8adff06ff731ce3b5a93a150b531d2ae0a62012d94dd96c9754ac0bcc8a7"
 
   def install
     bin.install "n26"
