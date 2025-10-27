c537b79d9e85bd1cf3e6214a2cf7bc5bd767bff6
Guilherme <thomazi@linux.com>
2025-10-27 10:30:26 +0100

    Update capable.rb to version 0.0.3

--- capable.rb
+++ capable.rb
@@ -1,9 +1,9 @@
 class Capable < Formula
   desc "Interact with Capacities.io API from your terminal"
   homepage "https://github.com/guitmz/capable"
-  url "https://github.com/guitmz/capable/releases/download/0.0.2/capable_0.0.2_darwin_arm64.tar.gz"
-  version "0.0.2"
-  sha256 "ed117193558ef4c921587afbdabf1761649dd3db166faf5d9eab3216e830b269"
+  url "https://github.com/guitmz/capable/releases/download/0.0.3/capable_0.0.3_darwin_arm64.tar.gz"
+  version "0.0.3"
+  sha256 "d89b33ec123f434207e2f8253df3762937f8765a18b5099e5b8e54e940358398"
 
   def install
     bin.install "capable"
